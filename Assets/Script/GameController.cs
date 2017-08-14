c11ebc8bbb51c997093e0cc90e20eb87b4365614
Chatchai Saratakij <CSaratakij@gmail.com>
2017-08-14 16:33:03 +0700

    Added WaveController in scene.

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public static bool isGameStart = false; // <- really?
+
+
 	void Start()
     {
 		
--- Assets/Script/WaveController.cs
+++ Assets/Script/WaveController.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class WaveController : MonoBehaviour
 {
-	void Start()
+    [SerializeField]
+    CrowdController crowdController;
+
+
+    void Start()
     {
-		
-	}
+        if (crowdController) {
+            GameController.isGameStart = true;
+        }
+    }
 
 	void Update()
     {
-		
+        if (GameController.isGameStart) {
+            _HandleWavePattern();
+        }
 	}
+
+    void _HandleWavePattern()
+    {
+        Debug.Log("About to wave...");
+        var pos = new Vector2(0, 0);
+
+        for (int i = 0; i < crowdController.CrowdObjects.Length; i++) {
+            pos.x = i;
+
+            for (int j = 0; j < crowdController.CrowdObjects[i].Length; j++) {
+                pos.y = j;
+
+                if (crowdController.HideSpritePos.Contains(pos)) {
+                    continue;
+                }
+
+                var currentCrowd = crowdController.CrowdObjects[i][j].GetComponent<Crowd>();
+                currentCrowd.HandUp();
+            }
+        }
+    }
 }
